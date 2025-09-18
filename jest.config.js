c5e72ebaf6c78071aeca974c4a1ef851e054d3a1
Mohamed Ben <b.mohamed@tomba.io>
2025-09-18 15:15:46 +0100

    Add Jest configuration file for TypeScript testing setup

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+    },
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testMatch: ["**/tests/**/*.test.ts"],
+};
